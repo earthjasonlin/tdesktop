22c2bccec00c5474f78d50a514afee666035c4e0
Zichao Lin <earthjasonlin@163.com>
2025-02-22 09:32:35 +0800

    forces the program to always recognize the user as premium

--- Telegram/SourceFiles/main/main_session.cpp
+++ Telegram/SourceFiles/main/main_session.cpp
@@ -263,7 +263,7 @@ rpl::producer<> Session::downloaderTaskFinished() const {
 }
 
 bool Session::premium() const {
-	return _user->isPremium();
+	return true;
 }
 
 bool Session::premiumPossible() const {
